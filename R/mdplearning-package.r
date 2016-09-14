f73f769bb153e6f98d4c4663ca9540d569766679
Carl Boettiger <cboettig@gmail.com>
2016-09-14 02:27:06 +0000

    fix for data checks

--- R/mdplearning-package.r
+++ R/mdplearning-package.r
@@ -3,3 +3,11 @@
 #' @name mdplearning
 #' @docType package
 NULL
+
+#' This is data to be included in my package
+#'
+#' @name scaled_data
+#' @docType data
+#' @author Carl Boettiger
+#' @keywords data
+NULL
