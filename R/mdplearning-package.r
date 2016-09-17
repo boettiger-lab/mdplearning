5b8c5621de38f121e62ac584a33567ec69777ac9
Carl Boettiger <cboettig@gmail.com>
2016-09-17 05:50:16 +0000

    update docs for data

--- R/mdplearning-package.r
+++ R/mdplearning-package.r
@@ -11,3 +11,11 @@ NULL
 #' @author Carl Boettiger
 #' @keywords data
 NULL
+
+#' This is data to be included in my package
+#'
+#' @name bluefin_tuna
+#' @docType data
+#' @author Carl Boettiger
+#' @keywords data
+NULL
